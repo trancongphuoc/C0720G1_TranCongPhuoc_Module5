dfc866dd731dcfcfef2bbb218a4b278118e69167
tran cong phuoc <teosake.1999@gmail.com>
2020-12-23 17:09:29 +0700

    ---

---
+++ 02_type_script/practices/variable_and_datatypes/variable_and_datatypes.ts
@@ -0,0 +1,81 @@
+// (function main() {
+//     console.log("START");
+//     if (true) {
+//         var lang = "vi";
+//         let target = "en-us";
+//         console.log("inside block");
+//         console.log(target);
+//     }
+//     console.log(lang);
+// })();
+//
+//
+//
+// (function main() {
+//     var x = 5;
+//     console.log(x);
+//     var x = 10;
+//     console.log(x);
+//
+//     let y = 100;
+//     console.log(y);
+// })();
+//
+//
+//
+// (function main() {
+//     console.log(x);
+//     var x = 5;
+//
+//     let y = 10;
+//     console.log(y);
+// })();
+
+
+(function main() {
+    let message: string;
+    let total: number = 100;
+    let isProduction = true;
+    let prices: Array<number> = [120, 88, 60];
+    let languages: string[] = ['vi', 'en-us'];
+    let now = new Date();
+    let unknown: any;
+
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
+
+    function log(msg: string): void {
+        console.log(msg)
+    }
+
+    interface IPost {
+        id: string;
+        title: string;
+        body?: string;
+    }
+
+    isProduction = false;
+    unknown = Direction.UP;
+    unknown = 'changed';
+
+    const post: IPost = {
+        id: "", title: ""
+
+    };
+
+    message = "50";
+
+    function getPost(postId: string): IPost {
+        // do something to retrieve post
+        return {
+            id: postId,
+            title: 'Post Title',
+            body: 'Post Body',
+            extra: 'data'
+        } as IPost;
+    }
+})();
